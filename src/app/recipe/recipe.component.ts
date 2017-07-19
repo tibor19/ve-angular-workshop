fb106af90db3758f63a2d6750158cf035ace44fb
Tiberiu Covaci <tibi@covaci.se>
2017-07-20 00:56:58 +0300

    ViewChild Implemented

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AfterContentInit,  ViewChild } from '@angular/core';
+import {IngredientsComponent} from '../ingredients/ingredients.component';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
-export class RecipeComponent implements OnInit {
+export class RecipeComponent implements OnInit, AfterContentInit {
+
+  @ViewChild(IngredientsComponent)
+  private ingredientsComponent: IngredientsComponent;
 
   recipe: IRecipe = {
     'recipeId': 1,
@@ -64,6 +69,10 @@ export class RecipeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    this.ingredientsComponent.ingredients = this.recipe.recipeIngredients;
   }
 
 }
